84bc76099c55f3b3998ffc5d4342419fe3f81fce
etherell <kostyaetherell@gmail.com>
2023-11-08 10:45:50 +0200

    Added readme md

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id                            :bigint           not null, primary key
+#  additional_service_area_codes :text             default([]), is an Array
+#  digsite_info_text             :text             not null
+#  primary_service_area_code     :string           not null
+#  request_action                :string           not null
+#  request_number                :string           not null
+#  request_type                  :string           not null
+#  response_due_date_time        :datetime         not null
+#  sequence_number               :integer          not null
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#
 class Ticket < ApplicationRecord
   has_one :excavator, dependent: :destroy
 
--- test/factories/excavators.rb
+++ test/factories/excavators.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: excavators
+#
+#  id           :bigint           not null, primary key
+#  address      :string           not null
+#  company_name :string           not null
+#  crew_onsite  :boolean          default(FALSE), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  ticket_id    :bigint           not null
+#
+# Indexes
+#
+#  index_excavators_on_ticket_id  (ticket_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ticket_id => tickets.id)
+#
 FactoryBot.define do
   factory :excavator do
     company_name { FFaker::Lorem.word }
